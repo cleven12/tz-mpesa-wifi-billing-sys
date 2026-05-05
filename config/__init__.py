32cbd5cbe22d3d4b08144a279ce6e545da23e1c2
cleven12 <cleven.noreplay@gmail.com>
2026-05-05 16:49:58 +0300

    refactor(config): add CONFIG_MAP dict to config/__init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,11 @@
-"""Configuration package. Import config classes from here."""
+"""Configuration package."""
 
 from config.settings import DevelopmentConfig, ProductionConfig, TestingConfig
 
-__all__ = ["DevelopmentConfig", "ProductionConfig", "TestingConfig"]
+CONFIG_MAP = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig,
+}
+
+__all__ = ["DevelopmentConfig", "ProductionConfig", "TestingConfig", "CONFIG_MAP"]
