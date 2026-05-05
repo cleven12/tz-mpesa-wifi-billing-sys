add408e4ccf550a3a3c879a9d84b24b7db00672d
cleven12 <cleven.noreplay@gmail.com>
2026-05-05 16:49:58 +0300

    feat(config): add LOG_FORMAT and LOG_DATE_FORMAT constants to logging_config

--- config/logging_config.py
+++ config/logging_config.py
@@ -12,3 +12,6 @@ def configure_logging(app) -> None:
         app: The Flask application instance.
     """
     raise NotImplementedError("TODO")
+
+LOG_FORMAT = "%(asctime)s %(levelname)-8s %(name)s: %(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
